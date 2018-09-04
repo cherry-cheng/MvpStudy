8202700cb332fe64c6ddd2589b62f7ddfdbe3a2c
chengyonghui <chengyonghui@yizhanit.com>
2018-09-04 19:19:39 +0800

    去掉多余部分

--- bilibili/src/main/java/com/bilibili/ui/main/MainActivity.java
+++ bilibili/src/main/java/com/bilibili/ui/main/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.internal.NavigationMenuView;
 import android.support.design.widget.NavigationView;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 
@@ -47,14 +45,10 @@ public class MainActivity extends BaseActivity implements IBaseMvpActivity<MainP
     MainFragment mainFragment;
     @Inject
     RegionFragment regionFragment;
-    @BindView(R.id.main_drawer_layout)
-    DrawerLayout mDrawerLayout;
     @BindView(R.id.entrance_bar)
     BottomBar mBottomBar;
     @BindView(R.id.main_container)
     FrameLayout mFrameLayout;
-    @BindView(R.id.main_nav_view)
-    NavigationView mNavigationView;
 
     private SupportFragment[] mFragments = new SupportFragment[4];
     private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
@@ -84,18 +78,7 @@ public class MainActivity extends BaseActivity implements IBaseMvpActivity<MainP
     @Override
     public void initViewAndEvent() {
         StatusBarUtil.setColorForDrawerLayout(this, getResources().getColor(R.color.theme_color_primary), mFrameLayout);
-        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                mDrawerLayout.closeDrawer(GravityCompat.START);
-                return true;
-            }
-        });
-        //隐藏NavigationView右侧滚动条
-        NavigationMenuView navigationMenuView = (NavigationMenuView) mNavigationView.getChildAt(0);
-        if (navigationMenuView != null) {
-            navigationMenuView.setVerticalScrollBarEnabled(false);
-        }
+
         mTitles = getResources().getStringArray(R.array.main_sections);
         for (int i = 0; i < mTitles.length; i++) {
             mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
@@ -150,15 +133,12 @@ public class MainActivity extends BaseActivity implements IBaseMvpActivity<MainP
 
     @Override
     public void onBackPressedSupport() {
-        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else {
             if (getSupportFragmentManager().getBackStackEntryCount() > 1) {
                 pop();
             } else {
                 finish();
             }
-        }
+
     }
 
     /**
@@ -166,10 +146,6 @@ public class MainActivity extends BaseActivity implements IBaseMvpActivity<MainP
      */
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onEvent(ToggleDrawerEvent event) {
-        if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-            mDrawerLayout.closeDrawer(GravityCompat.START);
-        } else {
-            mDrawerLayout.openDrawer(GravityCompat.START);
-        }
+
     }
 }
