ec509d969028a10d4df661be2764f54842b3bdf7
chengyonghui <chengyonghui@yizhanit.com>
2018-09-17 11:26:38 +0800

    可以播放但需调整

--- app/src/main/java/com/weizhan/superlook/ui/recommend1/viewbinder/Recommend1BodyItemViewBinder.java
+++ app/src/main/java/com/weizhan/superlook/ui/recommend1/viewbinder/Recommend1BodyItemViewBinder.java
@@ -1,6 +1,7 @@
 package com.weizhan.superlook.ui.recommend1.viewbinder;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -17,6 +18,8 @@ import com.common.util.ToastUtils;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.weizhan.superlook.R;
 import com.weizhan.superlook.model.bean.recommend1.AppRecommend1Show;
+import com.weizhan.superlook.ui.play.PlayerActivity;
+import com.weizhan.superlook.util.Constants;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -37,7 +40,7 @@ public class Recommend1BodyItemViewBinder extends ItemViewBinder<AppRecommend1Sh
 
     @Override
     protected void onBindViewHolder(@NonNull Recommend1BodyItemViewBinder.Recommend1BodyViewHolder holder, @NonNull AppRecommend1Show.Body item) {
-        Context context = holder.ivCover.getContext();
+        final Context context = holder.ivCover.getContext();
 /*        if (holder.getPosition() == 5) {
             int width = ScreenUtil.getScreenWidth(context) / 3 - SystemUtil.dp2px(context, 8);
             int height = context.getResources().getDimensionPixelSize(R.dimen.recommend1_cover_height1);
@@ -64,6 +67,10 @@ public class Recommend1BodyItemViewBinder extends ItemViewBinder<AppRecommend1Sh
             @Override
             public void onClick(View view) {
                 ToastUtils.showLongToast("点击了影片");
+                Intent intent = new Intent(context, PlayerActivity.class);
+                intent.putExtra("url", Constants.PLAY_URL);
+                intent.putExtra("isLive", true);
+                context.startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/com/weizhan/superlook/util/Constants.java
@@ -0,0 +1,9 @@
+package com.weizhan.superlook.util;
+
+/**
+ * Created by Administrator on 2018/9/17.
+ */
+
+public class Constants {
+    public static String PLAY_URL = "https://135zyv4.xw0371.com/2018/07/30/ziPVzMXGIws51Hmn/playlist.m3u8";
+}
