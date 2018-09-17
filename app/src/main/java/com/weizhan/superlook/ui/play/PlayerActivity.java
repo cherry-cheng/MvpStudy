c82a70a4e46d5bb047200c8efcf6bc98f243bc43
chengyonghui <chengyonghui@yizhanit.com>
2018-09-17 13:50:56 +0800

    播放器行为

--- app/src/main/java/com/weizhan/superlook/ui/play/PlayerActivity.java
+++ app/src/main/java/com/weizhan/superlook/ui/play/PlayerActivity.java
@@ -3,6 +3,9 @@ package com.weizhan.superlook.ui.play;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
@@ -14,12 +17,7 @@ import com.dueeeke.videoplayer.player.IjkVideoView;
 import com.dueeeke.videoplayer.player.PlayerConfig;
 import com.weizhan.superlook.R;
 
-/**
- * 播放器演示
- * Created by Devlin_n on 2017/4/7.
- */
-
-public class PlayerActivity extends BaseActivity {
+public class PlayerActivity extends AppCompatActivity {
 
     private IjkVideoView ijkVideoView;
 
@@ -37,10 +35,6 @@ public class PlayerActivity extends BaseActivity {
         Intent intent = getIntent();
         if (intent != null) {
             StandardVideoController controller = new StandardVideoController(this);
-            /*boolean isLive = intent.getBooleanExtra("isLive", false);
-            if (isLive) {
-                controller.setLive();
-            }*/
             ijkVideoView.setPlayerConfig(new PlayerConfig.Builder()
                     .autoRotate()//自动旋转屏幕
 //                    .enableCache()//启用边播边存
@@ -54,14 +48,6 @@ public class PlayerActivity extends BaseActivity {
             ijkVideoView.setVideoController(controller);
             ijkVideoView.start();
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -83,59 +69,9 @@ public class PlayerActivity extends BaseActivity {
     }
 
     @Override
-    public void onBackPressedSupport() {
+    public void onBackPressed() {
         if (!ijkVideoView.onBackPressed()) {
             super.onBackPressed();
         }
-    }
-
-    public void screenScaleDefault(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_DEFAULT);
-    }
-
-    public void screenScale169(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_16_9);
-    }
-
-    public void screenScale43(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_4_3);
-    }
-
-    public void screenScaleOriginal(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_ORIGINAL);
-    }
-
-    public void screenScaleMatch(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_MATCH_PARENT);
-    }
-
-    public void screenScaleCenterCrop(View view) {
-        ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_CENTER_CROP);
-    }
-
-    int i = 0;
-    public void setMirrorRotate(View view) {
-        ijkVideoView.setMirrorRotation(i % 2 == 0);
-        i++;
-    }
-
-    public void setSpeed0_75(View view) {
-        ijkVideoView.setSpeed(0.75f);
-    }
-
-    public void setSpeed0_5(View view) {
-        ijkVideoView.setSpeed(0.5f);
-    }
-
-    public void setSpeed1_0(View view) {
-        ijkVideoView.setSpeed(1.0f);
-    }
-
-    public void setSpeed1_5(View view) {
-        ijkVideoView.setSpeed(1.5f);
-    }
-
-    public void setSpeed2_0(View view) {
-        ijkVideoView.setSpeed(2.0f);
     }
 }
