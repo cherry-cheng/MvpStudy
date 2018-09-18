f650d795edfdb1a20c23abef602469f8808e93c8
chengyonghui <chengyonghui@yizhanit.com>
2018-09-18 11:36:42 +0800

    播放界面

--- app/src/main/java/com/weizhan/superlook/ui/play/PlayerActivity.java
+++ app/src/main/java/com/weizhan/superlook/ui/play/PlayerActivity.java
@@ -4,14 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.KeyEvent;
-import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-
-import com.common.base.BaseActivity;
 import com.dueeeke.videocontroller.StandardVideoController;
 import com.dueeeke.videoplayer.player.IjkVideoView;
 import com.dueeeke.videoplayer.player.PlayerConfig;
@@ -47,7 +41,6 @@ public class PlayerActivity extends AppCompatActivity {
 //            ijkVideoView.setScreenScale(IjkVideoView.SCREEN_SCALE_CENTER_CROP);
             ijkVideoView.setUrl(intent.getStringExtra("url"));
             ijkVideoView.setVideoController(controller);
-
             ijkVideoView.start();
         }
     }
